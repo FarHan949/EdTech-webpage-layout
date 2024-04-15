30b11445d8a2a963356867a3a6f64172a0317c99
FarHan949 <uddinfarhan949@gmail.com>
2024-04-15 08:17:06 +0600

    uploaded

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('read-btn').addEventListener('click',function(){
+    console.log('clicked')
+  const dNone =  document.getElementById('none')
+  dNone.removeAttribute('d-none')
+})
